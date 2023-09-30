9d81e500fd0a09660294e3eb21058413c4658b51
tezze <tezzer@inbox.ru>
2023-10-01 02:08:06 +0300

    keras.layers.Denso to keras.layers.Conv1D

--- main.py
+++ main.py
@@ -16,41 +16,40 @@ def restore_signal(distorted_signal, noise):
     noise = tf.expand_dims(noise, axis=0)
 
     model = tf.keras.Sequential([
-        tf.keras.layers.Dense(64, activation='linear', input_shape=(distorted_signal.shape[1],)),
-        tf.keras.layers.Dense(distorted_signal.shape[1], activation='linear')
+        tf.keras.layers.Conv1D(1, 640, 1,input_shape=(distorted_signal.shape[1],1), padding='same')
     ])
 
     model.compile(loss='mean_squared_error', optimizer='adam')
-    model.fit(noise, distorted_signal, epochs=10000, batch_size=128)
-    restored_signal = model.predict(noise)
+    model.fit(noise,distorted_signal, epochs=1000, batch_size=640)
+    restored_signal = model.predict(distorted_signal)
 
     # Сохраняем модель
-    model.save("my_model_10000")
+    #model.save("my_model_10000")
 
     return restored_signal
 
 distorted_signal = data[0]
 noise = data[1]
-# restored_signal = restore_signal(distorted_signal, noise)
-#
-# print(restored_signal)
-#
-# print(data)
-# fig = data.plot()
-# data2 = pd.DataFrame()
-# data2.insert(0, 0, restored_signal[0])
-# data2.insert(1, 1, data[1])
-# fig3 = data2.plot()
-# plt.show()
+restored_signal = restore_signal(distorted_signal, noise)
 
+print(restored_signal)
 
-loaded_model = tf.keras.models.load_model("my_model")
-noise = tf.expand_dims(noise, axis=0)
-restored_signal_from_loaded_model = loaded_model.predict(noise)
 print(data)
 fig = data.plot()
 data2 = pd.DataFrame()
-data2.insert(0, 0, restored_signal_from_loaded_model[0])
+data2.insert(0, 0, restored_signal[0].reshape(-1))
 data2.insert(1, 1, data[1])
 fig3 = data2.plot()
 plt.show()
+
+
+# loaded_model = tf.keras.models.load_model("my_model")
+# noise = tf.expand_dims(noise, axis=0)
+# restored_signal_from_loaded_model = loaded_model.predict(noise)
+# print(data)
+# fig = data.plot()
+# data2 = pd.DataFrame()
+# data2.insert(0, 0, restored_signal_from_loaded_model[0])
+# data2.insert(1, 1, data[1])
+# fig3 = data2.plot()
+# plt.show()
